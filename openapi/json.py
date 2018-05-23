7dea53e02d88d932dae144e72c42e8737ab68cae
Conor Stevenson <c.stevenson@outlook.com>
2018-05-23 14:02:05 +0100

    Minor json changes

--- openapi/json.py
+++ openapi/json.py
@@ -7,13 +7,13 @@ import simplejson
 
 def encoder(obj):
     if isinstance(obj, UUID):
-        return int(obj)
+        return obj.hex
     if isinstance(obj, datetime):
         return obj.isoformat()
     raise TypeError
 
 
 loads = partial(simplejson.loads, use_decimal=True)
-dumps = partial(simplejson.dumps, use_decimal=True, default=encoder)
+dumps = partial(simplejson.dumps, use_decimal=True, default=encoder, iterable_as_array=True)
 
 __all__ = ['loads', 'dumps']
