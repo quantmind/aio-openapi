efa2d4c30b58e1732a7b4c278d872f3dec44cf2a
Conor Stevenson <conor@lendingblock.com>
2018-05-31 11:46:32 +0100

    Common db setup for tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,59 +1,8 @@
-import os
-import asyncio
 from datetime import datetime
-
-import pytest
 
 from click.testing import CliRunner
 
-from aiohttp import test_utils
-
-from openapi.rest import rest
-from openapi.json import dumps
-from openapi.db.utils import create_database, create_tables
-from openapi import db
 from openapi.testing import jsonBody
-
-from . import example
-
-
-DEFAULT_DB = 'postgres://postgres:postgres@localhost:5432/postgres'
-
-
-def setup_app(app):
-    db.setup_app(app)
-    example.setup_app(app)
-
-
-@pytest.fixture(scope='session')
-def loop():
-    """Return an instance of the event loop."""
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest.fixture(scope='session')
-def test_app():
-    if not os.environ.get('DATASTORE'):
-        os.environ['DATASTORE'] = DEFAULT_DB
-    cli = rest(setup_app=setup_app)
-    cli.load_dotenv()
-    app = cli.web()
-    engine = app.get('store')
-    if engine:
-        app['store'] = create_database(app, 'openapi_unit_test')
-        create_tables(app)
-    return app
-
-
-@pytest.fixture(scope='session')
-async def cli(loop, test_app):
-    server = test_utils.TestServer(test_app, loop=loop)
-    client = test_utils.TestClient(server, loop=loop, json_serialize=dumps)
-    await client.start_server()
-    yield client
-    await client.close()
 
 
 def test_db(cli):
