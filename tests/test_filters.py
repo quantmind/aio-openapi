8bb68df179993643951e124964ef4c3e89983c06
Conor Stevenson <conor@lendingblock.com>
2018-06-29 09:44:12 +0100

    Test filter NULL

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -17,7 +17,7 @@ async def test_spec(test_app):
     }
 
 
-async def test_filters(cli):
+async def test_filters(cli, clean_db):
     test1 = {
         'title': 'test1',
         'severity': 1
@@ -26,9 +26,13 @@ async def test_filters(cli):
         'title': 'test2',
         'severity': 3
     }
+    test3 = {
+        'title': 'test3'
+    }
 
     await cli.post('/tasks', json=test1)
     await cli.post('/tasks', json=test2)
+    await cli.post('/tasks', json=test3)
 
     async def assert_query(params, expected):
         response = await cli.get('/tasks', params=params)
@@ -41,3 +45,4 @@ async def test_filters(cli):
     await assert_query({'severity:lt': 2}, [test1])
     await assert_query({'severity': 2}, [])
     await assert_query({'severity': 1}, [test1])
+    await assert_query({'severity': 'NULL'}, [test3])
