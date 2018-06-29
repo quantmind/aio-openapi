705d963ee288134a96d52c93b7d7a6be398de4d1
Luca Sbardella <luca@quantmind.com>
2018-06-29 18:34:24 +0100

    bump coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from click.testing import CliRunner
 
 from openapi.rest import rest
@@ -15,3 +16,14 @@ def test_version():
     result = runner.invoke(rest(), ['--version'])
     assert result.exit_code == 0
     assert result.output.startswith('Open API')
+
+
+def test_serve():
+    runner = CliRunner()
+    cli = rest(base_path='/v1')
+    with patch('aiohttp.web.run_app') as mock:
+        result = runner.invoke(cli, ['serve'])
+        assert result.exit_code == 0
+        assert mock.call_count == 1
+        app = mock.call_args[0][0]
+        assert app.router
