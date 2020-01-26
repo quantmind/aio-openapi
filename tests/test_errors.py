cab81e93c8c0452c5b7897c02779538a9ba57da7
Luca Sbardella <luca@quantmind.com>
2020-01-26 21:23:43 +0000

    test servers

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -2,7 +2,7 @@ from openapi.spec.path import BAD_DATA_MESSAGE
 from openapi.testing import jsonBody
 
 
-async def test_eroor1(cli):
+async def test_bad_data(cli):
     for bad in ([1], 3, "ciao"):
         response = await cli.post("/tasks", json=bad)
         data = await jsonBody(response, 400)
